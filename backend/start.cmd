edd45e073a5d8b3f621c7d2edc6f4e1f6442b80f
naveen <naveendav2000@gmail.com>
2024-09-02 16:46:29 +0530

    delete start

--- backend/start.cmd
+++
@@ -1,6 +0,0 @@
-set DATABASE=b04mlo7c0bmowxp0rreu
-set USERNAME=u26ubnxgnjlbdgii
-set PASSWORD=PWMt46M3eTKnSkfTh5ls
-set HOST=b04mlo7c0bmowxp0rreu-mysql.services.clever-cloud.com
-set PORT=8080
-node index.js
